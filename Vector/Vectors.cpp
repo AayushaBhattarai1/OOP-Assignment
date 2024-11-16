1cd0c1e0b501d83f31de03f4f49e23df0567054e
AayushaBhattarai1 <113623787+AayushaBhattarai1@users.noreply.github.com>
2024-11-16 21:53:15 +0545

    Add files via upload

---
+++ Vector/Vectors.cpp
@@ -0,0 +1,25 @@
+// Verify all letters from 2nd string appear in the front 
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+bool containsAllLetters(const string &first, const string &second) {
+    unordered_set<char> letterSet(first.begin(), first.end());
+    
+    for (char ch : second) {
+        if (letterSet.find(ch) == letterSet.end()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string str1 = "Python";
+    string str2 = "Py";
+    
+    bool result = containsAllLetters(str1, str2);
+    cout << "First string contains all letters from second string: " << (result ? "true" : "false") << endl;
+    
+    return 0;
+}
